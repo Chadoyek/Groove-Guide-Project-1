c29d4ef1458a2d3993698eeee796a02c8a7663b5
Chandrika Sista <cguntur@gmail.com>
2024-03-20 23:26:08 -0700

    Got the api working with the genre, min and max popularity variables. Got part of the generate cards working

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,18 +52,41 @@ rangeInput.forEach(input =>{
 $(document).foundation();
 //snagged from spotify's API doc listed here: 
 //https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
-var minPop = document.querySelector('#sliderOutput1'); //variable for minimum popularity //we still need to gather this data, it isnt being done right now
-var maxPop = document.querySelector('#sliderOutput2');//variable for maximum popularity // we still need to gather this data, it isnt being done right now
-var tracksSaved = document.querySelector('#tracksSaved'); //placeholder variable for tracks saved //Feature to add later? 
-
-
 
 var client_id = '3609e8db73c940d59d8b0dd1d47e0dd7';
 var client_secret = '73ca7f76b05e435a933602925a2392e7';
 var url = 'https://accounts.spotify.com/api/token';
 var form = new URLSearchParams({grant_type: 'client_credentials'});
+
+/* Element Selectors */
+var minPopRangeEl = document.querySelector(".range-min");
+var maxPopRangeEl = document.querySelector(".range-max");
 var selectEl = document.getElementById("genreSelect");
-console.log(selectEl);
+var fetchButton = document.getElementById("fetchButton");
+var accordionEl = document.getElementById("accordion-ul");
+
+
+var tracksSaved = document.querySelector('#tracksSaved'); //placeholder variable for tracks saved //Feature to add later? 
+
+/* Global Variables */
+var selectedGenre = "";
+var minPop = document.querySelector(".input-min").value;
+var maxPop = document.querySelector(".input-max").value;
+
+selectEl.addEventListener('change', function() {
+  selectedGenre = this.value;
+});
+
+minPopRangeEl.addEventListener('change', function(){
+  minPop = this.value;
+  console.log("Selected Min Pop: " + minPop);
+});
+
+maxPopRangeEl.addEventListener('change', function(){
+  maxPop = this.value;
+  console.log("Selected Max Pop: " + maxPop);
+});
+
 
 var authOptions = {
   method: "POST",
@@ -82,7 +105,6 @@ fetch(url, authOptions).then (function(response) {
     token = body.access_token;
     console.log(token)
     getGenres(token);
-   // getApi(token);
   })
   return;
   }
@@ -100,7 +122,6 @@ fetch(url, authOptions).then (function(response) {
  //}
 
 function getGenres() {
-    
   //get the genres
     var requestUrl = 'https://api.spotify.com/v1/recommendations/available-genre-seeds'
 
@@ -114,33 +135,27 @@ function getGenres() {
       })
       .then(function (data) {
         //Using console.log to examine the data
-        console.log(data);
 
         Object.entries(data).forEach(([key, value]) => {
-           console.log(key);
-           console.log(value);
-           
           for (var i = 0; i < value.length; i++) {
             //populate the genre variables based on fetch request
             var optionEl = document.createElement("option");
             optionEl.textContent = value[i];
+            optionEl.value = value[i];
             selectEl.append(optionEl)            
           }
-          
         });
-        
-
       });
   }
 
-//manually set variables for testing
-genre = "country"
-minPop = 0
-maxPop = 100
 
-function getApi() {
+//minPop = 0;
+//maxPop = 100;
+
+function getApi(selectedGenre, minPop, maxPop) {
    //get the API with user selected stuff
-   var requestUrl = 'https://api.spotify.com/v1/recommendations?seed_genres=' + genre + '&&min_popularity='+ minPop + '&max_popularity='+ maxPop;
+   var requestUrl = 'https://api.spotify.com/v1/recommendations?seed_genres=' + selectedGenre + '&&min_popularity='+ minPop + '&max_popularity='+ maxPop;
+   console.log("Request URL: " + requestUrl);
    
    fetch(requestUrl, {
      headers: {
@@ -153,18 +168,46 @@ function getApi() {
      .then(function (data) {
        //Using console.log to examine the data
        console.log(data);
-       for (var i = 0; i < data.length; i++) {
-         //populate the card variables based on fetch request
-         //generate the cards
-         
-       }
+       generateArtistCards(data);
      });
  }
-fetchButton.addEventListener('click', getApi);
+
+function generateArtistCards(data){
+  accordionEl.replaceChildren();
+  for (var i = 0; i < data.tracks.length; i++) {
+    console.log("i: " + i);
+     //populate the card variables based on fetch request
+     //generate the cards
+
+     var accordionLi = document.createElement("li");
+     accordionLi.classList.add("accordion-item", "is-active");
+     accordionLi.setAttribute('data', "accordion-item");
+
+     var accordionTitle = document.createElement("a");
+     accordionTitle.classList.add("accordion-title");
+     accordionTitle.textContent = data.tracks[i].artists[i].name;
+
+     accordionEl.append(accordionLi);
+     accordionLi.append(accordionTitle);
+
+   }
+
+  //<li class="accordion-item is-active" data-accordion-item>
+  //  <a href="#" class="accordion-title">Accordion 1</a>
+  //  <div class="accordion-content" data-tab-content >
+  //    <p>Panel 1. Lorem ipsum dolor</p>
+  //  </div>
+  //</li>
+
+}
+fetchButton.addEventListener('click', function(){
+  if(selectedGenre === ""){
+    alert("Please select a Genre to continue!");
+  }
+  getApi(selectedGenre, minPop, maxPop);
+});
 
 //testing our set genre & slider data.
-console.log( "your genre is: " + genre, "and your popularity settings are: "+ popLow, " and "+ popHigh + "." );
-
 // how the request should look
  //curl --request GET \
  // --url 'https://api.spotify.com/v1/recommendations?seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA' \
