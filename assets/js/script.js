90cda52274fd712dfdfd2ccde7e3aed3a7476f0f
Chandrika Sista <cguntur@gmail.com>
2024-03-21 21:01:58 -0700

    Added the modal for missing genre, deleted the accordion html

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,16 +56,14 @@ rangeInput.forEach(input =>{
   })
 })
 
-$(document).ready(function(){
-  $(document).foundation();
-});
+
 //snagged from spotify's API doc listed here: 
 //https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
 
 var client_id = '3609e8db73c940d59d8b0dd1d47e0dd7';
-// var client_id = 'df9aa5598206496d8f260a29f1738f45';
+//var client_id = 'df9aa5598206496d8f260a29f1738f45';
 var client_secret = '73ca7f76b05e435a933602925a2392e7';
-// var client_secret = '4b888695abc84c6aaa541ff00b7a5381';
+//var client_secret = '4b888695abc84c6aaa541ff00b7a5381';
 var url = 'https://accounts.spotify.com/api/token';
 var form = new URLSearchParams({grant_type: 'client_credentials'});
 
@@ -74,7 +72,6 @@ var minPopRangeEl = document.querySelector(".range-min");
 var maxPopRangeEl = document.querySelector(".range-max");
 var selectEl = document.getElementById("genreSelect");
 var fetchButton = document.getElementById("fetchButton");
-//var accordionEl = document.getElementById("accordion-ul");
 var accordionEl = document.querySelector(".accordion");
 var resultsListEl = document.querySelector("#results");
 var savedArtists = JSON.parse(localStorage.getItem("Artists"));
@@ -88,6 +85,24 @@ var tracksSaved = document.querySelector('#tracksSaved'); //placeholder variable
 var selectedGenre = "";
 var minPop = document.querySelector(".input-min").value;
 var maxPop = document.querySelector(".input-max").value;
+
+$(document).ready(function(){
+  $(document).foundation();
+
+  // this initializes the dialog (and uses some common options that I do)
+  $("#dialog").dialog({
+    autoOpen : false, modal : true, show : "blind", hide : "blind"
+  });
+
+  $("#fetchButton").click(function(){
+    var isConfirmed = true;
+    if(selectedGenre === ""){
+      isConfirmed = false;
+      $( "#dialog" ).dialog("open");
+    }
+    getApi(selectedGenre, minPop, maxPop);
+  });
+});
 
 selectEl.addEventListener('change', function() {
   selectedGenre = this.value;
@@ -157,7 +172,7 @@ function getGenres() {
       for (var i = 0; i < value.length; i++) {
         //populate the genre variables based on fetch request
         var optionEl = document.createElement("option");
-        optionEl.textContent = value[i];
+        optionEl.textContent = value[i].toUpperCase();
         optionEl.value = value[i];
         selectEl.append(optionEl)            
       }
@@ -193,8 +208,6 @@ function generateArtistCards(data){
     var songUrl    = data.tracks[i].external_urls.spotify;
 
     var resultsLi = document.createElement("li");
-    //resultsLi.classList.add("accordion-item");
-    //resultsLi.setAttribute("data", "accordion-item");
     
     resultsLi.innerHTML = "<a href='"+spotifyUrl+"' target='_blank'>"+artistName+"</a>";
 
@@ -210,84 +223,8 @@ function generateArtistCards(data){
     saveButton.setAttribute("data-songUrl", songUrl);
     resultsListEl.appendChild(saveButton);
     
-    //resultsListEl.append(resultsSong);
   }
-
-  // //accordionEl.replaceChildren();
-  // console.log(data);
-  // resultsListEl.replaceChildren();
-  // Object.entries(data.tracks).forEach(([key, value]) => {
-  //   var artistsArray = value.artists;
-  //   console.log (value);
-  //   //for(i=0; i<artistsArray.length; i++){
-  //   for(i=0; i<10; i++){
-  //     var artistName = artistsArray[i].name;
-  //     var spotifyUrl = artistsArray[i].external_urls.spotify;
-  //     //var artistSong = artistsArray[i].name; //to be added
-
-  //     //populate the card variables based on fetch request
-  //     //generate the cards
-
-  //     var resultsLi = document.createElement("p");
-  //     //resultsLi.classList.add("accordion-item");
-  //     //resultsLi.setAttribute("data", "accordion-item");
-  //     resultsListEl.append(resultsLi);
-  //     resultsLi.innerHTML = "<a href='"+spotifyUrl+"' target='_blank'>"+artistName+"</a>";
-  //     //resultsLi.append(artistName);
-  //     //var resultsSong = document.createElement("li");
-  //     //resultsListEl.append(resultsSong);
-  //     //resultsSong.innerHTML ="<a href='"+spotifyUrl+"' target='_blank'>"+artistSong+"</a>";
-      
-  //   // var accordionLi = document.createElement("li");
-    // accordionLi.classList.add("accordion-item");
-    // accordionLi.setAttribute("data", "accordion-item");
-
-    // var accordionTitle = document.createElement("a");
-    // accordionTitle.classList.add("accordion-title");
-    // accordionTitle.textContent = artistName;
-
-    // var accordionDiv = document.createElement("div");
-    // accordionDiv.classList.add("accordion-content");
-    // accordionDiv.setAttribute("data", "data-tab-content");
-    // //accordionDiv.innerHTML = "<p"+spotifyUrl+"</p";
-    // accordionDiv.innerHTML = "<p<a href='"+spotifyUrl+"'Open in Spotify</a</p";
-
-    // accordionEl.append(accordionLi);
-    // accordionLi.append(accordionTitle);
-    // accordionLi.append(accordionDiv);
-
-   // }
-  //});
-  $('#accordion').html(accordionEl).accordion({collapsible: true}); 
-} 
-
-// function storeArtist() {
-//   localStorage.setItem("savedArtist", JSON.stringify(savedArtist));
-//   console.log(storeArtist);
-// }
-
-function renderSavedArtist() {
-  // loop through saved artist
 }
-
-// this will be the function called when saved btn is clicked 
-function saveNewArtist(e) {
-  var artistObj = {
-    artistName: e.target.dataset.artistName,
-    spotifyUrl: e.target.dataset.spotifyUrl,
-    recSong:  e.target.dataset.recSong,
-    songUrl: e.target.dataset.songUrl,
-  }
-  savedArtists.push(artistObj);
-  localStorage.setItem("Artists", JSON.stringify(savedArtists));
-}
-
-fetchButton.addEventListener('click', function(){
-  if(selectedGenre === ""){
-    alert("Please select a Genre to continue!");
-  }
-  getApi(selectedGenre, minPop, maxPop);
-});
 
 //testing our set genre & slider data.
 // how the request should look
